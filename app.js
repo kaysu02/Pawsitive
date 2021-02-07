cb08a0c7a8be5c63897b24adbd862cb0154a9827
Kawaii Doge <andrewmcneil9@gmail.com>
2021-02-07 02:46:19 -0600

    comments + color

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Use the StorageKey to access the data
+// Use the keys to access the data
 var storage = localStorage.getItem("StorageKey");
 var moniesStorage = localStorage.getItem("money");
 var unlockedStorage = localStorage.getItem("unlockedKey");
@@ -7,13 +7,11 @@ var unlockedStorage = localStorage.getItem("unlockedKey");
 if (storage !== null) {
   //converting JSON String -> Javascript object
   var data = JSON.parse(storage);
-  // load all data from the backend to the frontend.
   loadData(data);
-  //preparing the next todo item's index
   var id = data.length;
 }
 else {
-  // initialize the id to 0 and initialize data
+  // initialize the id to 0 and initialize data if null
   id = 0;
   data = [];
 }
@@ -52,7 +50,6 @@ document.body.onkeyup = function(e) {
     // adding it to the back end
     data.push({
       name: todo,
-      //trash: false,
       id: id
     });
     id = data.length;
@@ -63,10 +60,6 @@ document.body.onkeyup = function(e) {
 
 // function that creates the front-end HTML
 function newItem(todo, id) {
-  // if it's in the trash, do absolutely nothing
-  //if (trash == true) {
-  //  return;
-  //}
   var ul = document.getElementById("list");
 
   var li = document.createElement("li");
@@ -85,19 +78,13 @@ function newItem(todo, id) {
   li.onclick = removeItem;
 }
 
-// function to remove the todo from the frontend and backend
+// remove the item in the list that was clicked
 function removeItem(event) {
-  // THIS IS ALL FRONT END BELOW
-
-  // get the html code for what you have clicked
+  //FRONT END BELOW
   element = event.target;
-
-  //remove the html element after you click it
   element.remove();
 
-  // THIS IS BACKEND BELOW
-  // get the HTML id, find the trash in the backend, set trash property in data to true.
-  //data[element.id].trash = true;
+  //BACKEND BELOW
   console.log(element.id);
   var ul = document.getElementById("list");
   var items = ul.getElementsByTagName("li");
@@ -121,7 +108,8 @@ function removeItem(event) {
   localStorage.setItem("money", coins);
 }
 
-function unlockPet(petNum) //unsure of what type 'pet' is
+//if you have enough money, unlock the clicked pet
+function unlockPet(petNum)
 {
     var count = 0;
     for(var i = 0; i < unlockedPets.length; i++)
@@ -139,7 +127,7 @@ function unlockPet(petNum) //unsure of what type 'pet' is
         
         unlockedPets[petNum-1] = 1;
 
-        document.getElementById("item" + petNum).style.backgroundColor = "#b0d1cd";
+        document.getElementById("item" + petNum).style.backgroundColor = "#d3e0dc";
         var names = document.getElementsByName("price");
         for(var i = 0; i < names.length; i++){
             if(unlockedPets[i] == 1){
@@ -149,16 +137,18 @@ function unlockPet(petNum) //unsure of what type 'pet' is
                 names[i].innerHTML = "$" + price;
             }
         }
+        // set storage equal to newest changes
         localStorage.setItem("money", coins);
         localStorage.setItem("unlockedKey", JSON.stringify(unlockedPets));
     }
 }
 
+//upon loading the shop, set the backgrounds and prices to reflect previous purchases
 function loadBought(petNum)
 {
     for(var i = 1; i <= 9; i++){
         if(unlockedPets[i-1] == 1){
-            document.getElementById("item" + i).style.backgroundColor = "#b0d1cd";
+            document.getElementById("item" + i).style.backgroundColor = "#d3e0dc";
         }
     }
     var count = 0;
