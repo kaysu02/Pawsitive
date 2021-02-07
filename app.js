beeb1e69b69531be9d4387bddfd59786885ba560
Kawaii Doge <andrewmcneil9@gmail.com>
2021-02-06 18:55:17 -0600

    j

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (storage !== null) {
   //preparing the next todo item's index
   var id = data.length;
 
-  var coins = JSON.parse(moniesStorage);
+  var coins = 0;
   document.getElementById("moni").innerHTML = "coins: " + coins;
 }
 
